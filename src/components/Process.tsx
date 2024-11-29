d1c97f674dbc5a996ac07453142641bc4d4c5e04
svenschu <sve.sch@icloud.com>
2024-11-29 14:25:43 +0100

    First cloudflare deployment

--- src/components/Process.tsx
+++ src/components/Process.tsx
@@ -61,7 +61,7 @@ export default function Example() {
                 <div
                     className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
                     <video
-                        src="src/assets/talentlynx-process-animation.mp4"
+                        src="/talentlynx-process-animation.mp4"
                         className="w-full max-w-full rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10"
                         autoPlay
                         loop
