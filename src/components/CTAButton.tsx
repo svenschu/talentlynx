6283edb41ce33ce3331512d3c46abc1bac5d13fd
svenschu <sve.sch@icloud.com>
2024-11-29 16:30:38 +0100

    Second cloudflare deployment

--- src/components/CTAButton.tsx
+++ src/components/CTAButton.tsx
@@ -5,9 +5,6 @@ export default function CTAButton() {
         <button
             className="relative w-full sm:w-auto flex items-center justify-center gap-3 px-6 py-3 text-base font-mono font-bold text-white rounded-md bg-brand-aquamarine-500 transition-transform duration-300 hover:scale-105 hover:border hover:border-brand-primary-500 focus:outline-none"
         >
-            {/* Telefon-Icon */}
-            <PhoneIcon className="w-5 h-5 text-white" />
-
             {/* Text */}
             <span>Beratungstermin vereinbaren</span>
         </button>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,7 +15,7 @@ export default function Example() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
     return (
-        <div className="bg-brand-primary-700 text-brand-primary-50">
+        <div className="bg-brand-primary-900 text-brand-primary-50">
             <header className="absolute inset-x-0 top-0 z-50">
                 <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
                     <div className="flex lg:flex-1">
